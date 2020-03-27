a97ea3905c3bcac1785b95cd1f826d360f04f89a
Cao Mai <caomai@rocketmail.com>
2020-03-26 23:39:17 -0700

    [add] random number generator method

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,8 +17,15 @@ class MainActivity : AppCompatActivity() {
         rollButton.text = "Let's Roll"
 
         rollButton.setOnClickListener{
-            Toast.makeText(this, "button cliked", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, "button cliked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomValue = Random().nextInt(6) + 1
+        resultText.text = randomValue.toString()
     }
 }
 
